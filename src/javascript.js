ec13b9945996dbe681bfdc86afea0b1afb16b4cc
mafaldamac <mafaldamac@hotmail.com>
2021-02-07 19:34:52 +0000

    updated temperature units & current location info

--- src/javascript.js
+++ src/javascript.js
@@ -1,4 +1,4 @@
-// date and time - current
+// dDate & Time - Atual Info
 let now = new Date();
 let divCurrentInfo = document.getElementById("current-info");
 let currentDateElement = document.getElementById("current-date");
@@ -6,7 +6,7 @@ let currentHourElement = document.getElementById("current-hour");
 let date = now.getDate();
 let month = now.getMonth();
 let hours = now.getHours();
-let minutes = now.getMinutes() + "";
+let minutes = now.getMinutes();
 let monthNames = [
   "January",
   "February",
@@ -53,11 +53,13 @@ function search(event) {
 }
 
 function tempCitySearched(response) {
-  console.log(response.data);
   let city = (document.querySelector("#search-city").innerHTML =
     response.data.name);
+
+  celsiusTemp = response.data.main.temp;
+
   let temp = (document.querySelector("#temp-shown").innerHTML = Math.round(
-    response.data.main.temp
+    celsiusTemp
   ));
   let description = (document.querySelector("#description-weather").innerHTML =
     response.data.weather[0].description);
@@ -72,35 +74,21 @@ function tempCitySearched(response) {
     );
 }
 
-// degree  changes - C to F
-function ctoF(degreeCelsius) {
-  let degreesFar = degreeCelsius * 1.8 + 32;
-  return degreesFar.toFixed(1);
+// Temperature Units
+
+function showFahrTemperature(event) {
+  event.preventDefault();
+  let fahrTemperature = (celsiusTemp * 9) / 5 + 32;
+  let tempElement = document.querySelector("#temp-shown");
+  const val = Math.round(fahrTemperature);
+  tempElement.innerHTML = Math.round(fahrTemperature) + "";
 }
 
-function ftoC(degreeFarn) {
-  let deegresCelsius = (degreeFarn - 32) / 1.8;
-  return deegresCelsius.toFixed(1);
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temp-shown");
+  tempElement.innerHTML = Math.round(celsiusTemp);
 }
-
-function degreesChange() {
-  let celsiusAndFar = document.getElementById("celsius-link");
-  let tempDif = document.getElementById("temp-shown");
-  let units = celsiusAndFar.innerHTML;
-
-  if (units === "ºC") {
-    celsiusAndFar.innerHTML = "ºF";
-    let fahDegree = ctoF(Number(tempDif.innerHTML));
-    tempDif.innerHTML = fahDegree;
-  } else {
-    celsiusAndFar.innerHTML = "ºC";
-    let degreeCelsius = ftoC(Number(tempDif.innerHTML));
-    tempDif.innerHTML = degreeCelsius;
-  }
-}
-
-let celsTofah = document.getElementById("celsius-link");
-celsTofah.addEventListener("click", degreesChange);
 
 // Current Location
 
@@ -110,7 +98,20 @@ function locationTemp(response) {
   cityCurrentPosition.innerHTML = cityCurrent;
   let temperatureCurrentPosition = document.querySelector("#temp-shown");
   let temperatureCurrent = Math.round(response.data.main.temp);
+  celsiusTemp = temperatureCurrent;
   temperatureCurrentPosition.innerHTML = temperatureCurrent;
+  let iconCurrent = document
+    .querySelector("#icon")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  let descriptionCurrent = (document.querySelector(
+    "#description-weather"
+  ).innerHTML = response.data.weather[0].description);
+  let wind = (document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  ));
 }
 
 function currentLocation(position) {
@@ -127,5 +128,14 @@ function getCurrentPosition() {
 let button = document.querySelector("#current-button");
 button.addEventListener("click", getCurrentPosition);
 
+let celsiusTemp = null;
 let form = document.getElementById("form");
 form.addEventListener("submit", search);
+
+/////
+
+let fahrenheit = document.querySelector("#fahrenheit-link");
+fahrenheit.addEventListener("click", showFahrTemperature);
+
+let celsius = document.querySelector("#celsius-link");
+celsius.addEventListener("click", showCelsiusTemperature);
